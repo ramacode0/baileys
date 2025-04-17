0b82165af564e2286cbcf75abba9a82546486fea
ramacode.xyz <klopisprem@gmail.com>
2025-04-17 17:18:17 +0700

    Add files via upload

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,11 @@
-export * from './constants';
-export * from './encode';
-export * from './BinaryInfo';
+import makeWASocket from './Socket';
+export * from '../WAProto';
+export * from './Utils';
+export * from './Types';
+export * from './Store';
+export * from './Defaults';
+export * from './WABinary';
+export * from './WAM';
+export type WASocket = ReturnType<typeof makeWASocket>;
+export { makeWASocket };
+export default makeWASocket;
